bf1d1dbb4b94477b2b18d36c282556fe49b8245b
Mazur Iuliia <yuliya.vorona@gmail.com>
2025-01-15 18:10:12 +0100

    another streamlit

---
+++ testing_stramlit/app_graph.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import seaborn as sns
+
+list_datasets = sns.get_dataset_names()
+
+st.header("Data manipulation and dataviz")
+
+st.write("Which dataset are we going to use?")
+dataset  = st.selectbox("", list_datasets)
+
+df = sns.load_dataset(dataset)
+
+st.dataframe(df.head(20))
+
+x_axis  = st.selectbox("Now I want you to chose X axis", df.columns)
+y_axis  = st.selectbox("Without Y axis there is no fun. Please chose it as well", df.columns)
+graph = st.selectbox("Which type of graph are we going to use", 
+                          ["bar", "scatter", "line"])
+
+eval(f"st.{graph}_chart(df, x = x_axis, y = y_axis)")   
+
+matrix = st.checkbox('Do you want me to show you correlation matrix?')
+if matrix:
+    if (df[x_axis].dtype in ["float64", "int64"]) and (df[y_axis].dtype in ["float64", "int64"]):
+        sns.heatmap(df.corr(), x_axis, y_axis, annot=True)
+    else:
+        st.markdown(f"#### :red[You need to chose numeric columns. Chose between these {df.select_dtypes(['float64', 'int64']).columns.values}]")
